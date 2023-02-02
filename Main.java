6d7e602a4af90ef14c83276579b5d032a2e1e5c5
julietmg <88133519+julietmg@users.noreply.github.com>
2023-02-02 16:43:36 +0000

    Enabling simple scoring at the end of each round.

--- Main.java
+++ Main.java
@@ -1,38 +1,65 @@
 import java.io.FileNotFoundException;
 import java.util.*;
 
+import org.junit.platform.reporting.shadow.org.opentest4j.reporting.events.root.Finished;
+
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        Map<String, String> italianWords = DictionaryLoader.loadFromFile("Dictionaries/English_Italian.txt");
+        System.out.println("Hello! You're currently on level 1 of studying!");
 
-        String answer = "cat"; 
+        //User Maks  = new User("maksiu", 1); 
 
-        while (answer.isEmpty() == false) { 
+        Map<String, String> italianWords = DictionaryLoader.loadFromFile("Dictionaries/Level1_English_Italian.txt");
 
-        ArrayList<String> activeCard = new ArrayList<String>(italianWords.keySet());
-        String key = activeCard.get(new Random().nextInt(activeCard.size()));
-        System.out.println(key);
-
-        System.out.println("What's the answer?");
-        answer = System.console().readLine();
+        String answer = "cat";
+        int correctAnswer = 0;
+        int wrongAnswer = 0;
 
         
 
+        while (answer.isEmpty() == false) {
+
+            ArrayList<String> activeCard = new ArrayList<String>(italianWords.keySet());
+            String key = activeCard.get(new Random().nextInt(activeCard.size()));
+            System.out.println(key);
+
+            System.out.println("What's the answer?");
+            answer = System.console().readLine();
+
             if (italianWords.get(key).equalsIgnoreCase(answer)) {
                 System.out.println("This answer is correct!");
+                correctAnswer = correctAnswer + 1; 
             }
 
             else {
 
                 System.out.println("Unfortunately, this answer isn't correct.");
                 System.out.println("The correct answer was: " + italianWords.get(key) + ".");
+                wrongAnswer = wrongAnswer + 1; 
 
             }
 
         }
 
-    }
+       if (answer.isEmpty() == true) {
+        System.out.println("Your correct answers: " + correctAnswer);
+        System.out.println("Your wrong answers: " + wrongAnswer);
+       }
+
+       if (wrongAnswer > correctAnswer) {
+        System.out.println("You should practice more. Try to learn all these words by heart.");
+       }
+
+       else {
+        System.out.println("Good job! The next level is waiting for you. ");
+       }
+
+       
 
     }
+
+}
+
+
